fe41b5bbefb3e1d87d3634fe54d6fd9ac51199de
Ryan M <rmccauleymusic@gmail.com>
2019-10-28 01:32:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,8 +7,8 @@ def sieve(max)
   counter = 0
 
   primes.each do |p|
-    next unless p 
-    break if p*p > max 
+    next unless p
+    break if p*p > max
     counter += 1
 
     (p*p).step(max,p){ |m| primes[m] = nil}
